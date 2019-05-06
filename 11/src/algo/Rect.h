7a5be7a944ac4984bf58de885a9f962121215305
omeir1 <ohad.meir@intel.com>
2019-05-06 19:21:18 +0300

    src/algo/Rect.h - undelete....

---
+++ 11/src/algo/Rect.h
@@ -0,0 +1,18 @@
+#ifndef RECT_H_
+#define RECT_H_
+
+namespace FRVT_11 {
+    struct Rect {
+    Rect(int x1, int y1, int x2, int y2, float score): x1(x1), y1(y1), x2(x2), y2(y2), score(score) {}
+    
+    int Area()
+    {
+        return (x2 - x1 + 1) * (y2 - y1 + 1);
+    }
+    
+    int x1, y1, x2, y2;
+    float score;
+};
+}
+
+#endif /* RECT_H_ */
