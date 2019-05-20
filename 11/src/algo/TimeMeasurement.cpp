83481844c58a5143e9cf30a28073af09ee891064
perclnx415 <perclnx414@intel.com>
2019-05-20 11:25:38 -0400

    ValidateNumThreads

--- 11/src/algo/TimeMeasurement.cpp
+++ 11/src/algo/TimeMeasurement.cpp
@@ -1,6 +1,7 @@
 #include "TimeMeasurement.h"
 
 #include <iostream>
+#include <stdexcept>
 
 using namespace FRVT_11;
 
@@ -14,4 +15,24 @@ TimeMeasurement::Test()
     auto finish = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = finish - mStart;
     std::cout << "Elapsed time: " << elapsed.count() << " s" << std::endl;
+}
+
+std::string
+ValidateNumThreads()
+{
+    const char* cmd = "top -H -b -n1 | grep validate11 | wc -l";
+    char buffer[128];
+    std::string result = "";
+    FILE* pipe = popen(cmd, "r");
+    if (!pipe) throw std::runtime_error("popen() failed!");
+    try {
+        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
+            result += buffer;
+        }
+    } catch (...) {
+        pclose(pipe);
+        throw;
+    }
+    pclose(pipe);
+    return result;
 }
--- 11/src/algo/TimeMeasurement.h
+++ 11/src/algo/TimeMeasurement.h
@@ -1,6 +1,7 @@
 #ifndef TIMEMEASUREMENT_H_
 #define TIMEMEASUREMENT_H_
 
+#include <string>
 #include <chrono>
 
 namespace FRVT_11 {
@@ -13,5 +14,7 @@ private:
     std::chrono::time_point<std::chrono::high_resolution_clock> mStart;
 };
 }
+
+std::string ValidateNumThreads();
 
 #endif /* TIMEMEASUREMENT_H_ */
