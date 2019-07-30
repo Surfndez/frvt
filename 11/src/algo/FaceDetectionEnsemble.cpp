ae101fa04ec262c38dc5ee0c356cf8e91f6b49ab
perclnx415 <perclnx414@intel.com>
2019-07-30 08:21:14 -0400

    latest detection ensemble

--- 11/src/algo/FaceDetectionEnsemble.cpp
+++ 11/src/algo/FaceDetectionEnsemble.cpp
@@ -9,6 +9,7 @@ using namespace FRVT_11;
 FaceDetectionEnsemble::FaceDetectionEnsemble(const std::string &configDir)
 {
     mDetectors = {
+        std::make_shared<SsdFaceDetector>(configDir, "/facessd_mobilenet_v2_dm100_swish_512_0-527691", 512), // facessd_mobilenet_v2_dm100_swish_512_0-527691
         std::make_shared<SsdFaceDetector>(configDir, "/fd_tf_dm100_352_0-408944", 352), // facessd_mobilenet_v2_dm100_352_0-408944
         std::make_shared<SsdFaceDetector>(configDir, "/fd_tf_dm100_352_0-277768", 352), // facessd_mobilenet_v2_dm100_352_0-277768
     };
@@ -24,6 +25,7 @@ FaceDetectionEnsemble::Detect(const ImageData &imageData) const
         std::vector<Rect> rects = d->Detect(imageData);
         //std::cout << "Face detection: "; t.Test();
         if (rects.size() > 0) {
+            //std::cout << rects[0].x1 << " " << rects[0].y1 << " " << rects[0].x2 << " " << rects[0].y2 << std::endl;
             return {rects[0]};
         }
     }
