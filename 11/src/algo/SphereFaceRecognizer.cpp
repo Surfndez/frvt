d973c913a6a974678b4dcb3d6ed9867c1d543d4f
perclnx415 <perclnx414@intel.com>
2019-11-18 12:57:04 -0500

    Fix conversion rgb2bgr before recognition inference. this gives bit exact

--- 11/src/algo/SphereFaceRecognizer.cpp
+++ 11/src/algo/SphereFaceRecognizer.cpp
@@ -2,10 +2,9 @@
 
 #include <algorithm>
 #include <iostream>
+#include <fstream>
 #include <opencv2/core.hpp>
 #include <opencv2/imgproc.hpp>
-
-#include "../algo/TimeMeasurement.h"
 
 using namespace FRVT_11;
 
@@ -14,27 +13,29 @@ CropImage(const cv::Mat& image, const std::vector<int>& landmarks)
 {
     // Find min and max values from landmarks
 
-    std::vector<int> xPoints;
-    std::vector<int> yPoints;
+    std::vector<float> xPoints;
+    std::vector<float> yPoints;
     for (int i = 0; i < 10; i += 2) {
         xPoints.push_back(landmarks[i]);
         yPoints.push_back(landmarks[i+1]);
     }
 
-    int xMin = *std::min_element(xPoints.begin(), xPoints.end());
-    int xMax = *std::max_element(xPoints.begin(), xPoints.end());
-    int yMin = *std::min_element(yPoints.begin(), yPoints.end());
-    int yMax = *std::max_element(yPoints.begin(), yPoints.end());
-    
-    int w = (xMax - xMin);
-    int h = (yMax - yMin);
+    float xMin = *std::min_element(xPoints.begin(), xPoints.end());
+    float xMax = *std::max_element(xPoints.begin(), xPoints.end());
+    float yMin = *std::min_element(yPoints.begin(), yPoints.end());
+    float yMax = *std::max_element(yPoints.begin(), yPoints.end());
+
+    // std::cout << "\tMin/Max landmarks: " << xMin << " " << xMax << " " << yMin << " " << yMax << std::endl;
+     
+    float w = (xMax - xMin);
+    float h = (yMax - yMin);
 
     // Calculate bounding box
     
-    int x1 = xMin - int(w * 0.75);
-    int x2 = xMax + int(w * 0.75);
-    int y1 = yMin - int(h * 0.75);
-    int y2 = yMax + int(h * 0.75);
+    float x1 = xMin - (w * 0.75);
+    float x2 = xMax + (w * 0.75);
+    float y1 = yMin - (h * 0.75);
+    float y2 = yMax + (h * 0.75);
 
     // Keep original face ratio
 
@@ -42,20 +43,22 @@ CropImage(const cv::Mat& image, const std::vector<int>& landmarks)
     h = y2 - y1;
     if (h > w)
     {
-        int c = int((x1 + x2) / 2);
-        int half_w = int(h / 2);
+        float c = (x1 + x2) / 2;
+        float half_w = h / 2;
         x1 = c - half_w;
         x2 = c + half_w;
     }
     else
     {
-        int c = int((y1 + y2) / 2);
-        int half_h = int(w / 2);
+        float c = (y1 + y2) / 2;
+        float half_h = w / 2;
         y1 = c - half_h;
         y2 = c + half_h;
     }
     w = x2 - x1;
     h = y2 - y1;
+
+    // std::cout << "\tKeep ratio rect: " << x1 << " " << y1 << " " << x2 << " " << y2 << std::endl;
 
     // Check if need to pad image
     // std::vector<int> xy = {-x1, -y1, x2 + 1 - w, y2 + 1 - h};
@@ -70,14 +73,16 @@ CropImage(const cv::Mat& image, const std::vector<int>& landmarks)
     // }
 
     // TODO..... remove once padding is done
-    x1 = std::max(0, x1);
-    x2 = std::min(image.cols, x2);
-    y1 = std::max(0, y1);
-    y2 = std::min(image.rows, y2);
+    x1 = std::max(0.f, x1);
+    x2 = std::min(float(image.cols), x2);
+    y1 = std::max(0.f, y1);
+    y2 = std::min(float(image.rows), y2);
+
+    // std::cout << "\tCrop coords: " << image.rows << " " << image.cols << " -> " << int(x1) << " " << int(y1) << " " << int(x2) << " " << int(y2) << std::endl;
 
     // Crop
 
-    cv::Mat cropped = image(cv::Range(y1, y2), cv::Range(x1, x2));
+    cv::Mat cropped = image(cv::Range(int(y1), int(y2)), cv::Range(int(x1), int(x2)));
 
     return cropped;
 }
@@ -88,21 +93,29 @@ NormalizeImage(const cv::Mat& image, const std::vector<int>& landmarks)
     // crop
     cv::Mat cropped = CropImage(image, landmarks);
 
-    //std::cout << "cropped size: " << cropped.rows << " " << cropped.cols << std::endl;
+    // std::cout << "\tCropped size: " << cropped.rows << " " << cropped.cols << std::endl;
 
     // resize
-    cv::resize(cropped, cropped, cv::Size(128, 128), 0, 0, cv::INTER_LINEAR);
+    cv::resize(cropped, cropped, cv::Size(128, 128), 0, 0, cv::INTER_AREA);
 
     // to BGR
-    cv::cvtColor(image, image, cv::COLOR_RGB2BGR);
+    cv::cvtColor(cropped, cropped, cv::COLOR_RGB2BGR);
+
+    // double min_i, max_i;
+    // cv::minMaxLoc(cropped, &min_i, &max_i);
+    // std::cout << "\tmin-max " << min_i << "-" << max_i << std::endl;
     
     cropped.convertTo(cropped, CV_32FC3);
     cropped /= 255;
     cropped -= cv::Scalar(0.5, 0.5, 0.5);
 
-    //double min, max;
-    //cv::minMaxLoc(cropped, &min, &max);
-    //std::cout << "min-max " << min << "-" << max << std::endl;
+    // double min, max;
+    // cv::minMaxLoc(cropped, &min, &max);
+    // std::cout << "\tmin-max " << min << "-" << max << std::endl;
+
+    // cv::Scalar mean, stddev;
+    // cv::meanStdDev (cropped, mean, stddev);
+    // std::cout << "\tMean-Std: " << mean << " " << stddev << std::endl;
 
     return cropped;
 }
@@ -124,6 +137,12 @@ SphereFaceRecognizer::Infer(const ImageData& imageData, const std::vector<int>& 
     
     image = NormalizeImage(image, landmarks);
 
+    // cv::Mat flat = image.reshape(1, image.cols*image.rows*image.channels());
+    // std::vector<float> vec = image.isContinuous()? flat : flat.clone();
+    // std::ofstream fout("/home/administrator/nist/debug/data.dat", std::ios::out | std::ios::binary);
+    // fout.write((char*)&vec[0], vec.size() * 4);
+    // fout.close();
+
     // infer on original image
     auto output = mTensorFlowInference->Infer(image);
     float* output_features = static_cast<float*>(TF_TensorData(output[0].get()));
@@ -142,7 +161,7 @@ SphereFaceRecognizer::Infer(const ImageData& imageData, const std::vector<int>& 
         // features[i+512] = featuresMat_2.at<float>(i, 0);
     }
 
-    //std::cout << features[0] << " " << features[1] << std::endl;
+    // std::cout << "\tFeatures norm: " << cv::norm(featuresMat_1) << std::endl;
 
     return features;
 }
