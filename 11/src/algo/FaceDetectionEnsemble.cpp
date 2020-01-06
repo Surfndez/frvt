eba4e285ded5fea1aab2c47fdea17e01569ef760
perclnx415 <perclnx414@intel.com>
2020-01-06 16:21:51 -0500

    FaceDetectionEnsemble use single latest detector 416x416

--- 11/src/algo/FaceDetectionEnsemble.cpp
+++ 11/src/algo/FaceDetectionEnsemble.cpp
@@ -1,9 +1,6 @@
 #include "FaceDetectionEnsemble.h"
 #include "SsdFaceDetector.h"
 
-#include <iostream>
-#include "../algo/TimeMeasurement.h"
-
 using namespace FRVT_11;
 
 FaceDetectionEnsemble::FaceDetectionEnsemble(const std::string &configDir)
@@ -11,6 +8,7 @@ FaceDetectionEnsemble::FaceDetectionEnsemble(const std::string &configDir)
     mDetectors = {
         std::make_shared<SsdFaceDetector>(configDir, "/ssdlite_mobilenet_v3_large_416x416_fddb_wider_tasqai_noface2_filter10_0-400000", 416),
         // std::make_shared<SsdFaceDetector>(configDir, "/ssdlite_mobilenet_v3_large_224x224_fddb_wider_tasqai_noface2_filter20_steps_0-300000", 224),
+        // std::make_shared<SsdFaceDetector>(configDir, "/facessd_mobilenet_v2_dm100_swish_512x512_wider_filter10_0-527691", 512),
     };
 }
 
@@ -20,11 +18,8 @@ std::vector<Rect>
 FaceDetectionEnsemble::Detect(const ImageData &imageData) const
 {
     for (auto& d : mDetectors) {
-        //auto t = TimeMeasurement();
         std::vector<Rect> rects = d->Detect(imageData);
-        //std::cout << "Face detection: "; t.Test();
         if (rects.size() > 0) {
-            //std::cout << rects[0].x1 << " " << rects[0].y1 << " " << rects[0].x2 << " " << rects[0].y2 << std::endl;
             return {rects[0]};
         }
     }
