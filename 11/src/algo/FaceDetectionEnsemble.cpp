2344ce6e4f6fac66787deb6028434ef949e58d5a
perclnx415 <perclnx414@intel.com>
2020-01-07 19:48:49 -0500

    Detection ensemble new+old

--- 11/src/algo/FaceDetectionEnsemble.cpp
+++ 11/src/algo/FaceDetectionEnsemble.cpp
@@ -7,8 +7,7 @@ FaceDetectionEnsemble::FaceDetectionEnsemble(const std::string &configDir)
 {
     mDetectors = {
         std::make_shared<SsdFaceDetector>(configDir, "/ssdlite_mobilenet_v3_large_416x416_fddb_wider_tasqai_noface2_filter10_0-400000", 416),
-        // std::make_shared<SsdFaceDetector>(configDir, "/ssdlite_mobilenet_v3_large_224x224_fddb_wider_tasqai_noface2_filter20_steps_0-300000", 224),
-        // std::make_shared<SsdFaceDetector>(configDir, "/facessd_mobilenet_v2_dm100_swish_512x512_wider_filter10_0-527691", 512),
+        std::make_shared<SsdFaceDetector>(configDir, "/facessd_mobilenet_v2_dm100_swish_512x512_wider_filter10_0-527691", 512)
     };
 }
 
